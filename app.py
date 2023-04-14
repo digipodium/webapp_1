4cea3750cf58356e0dd71ae12a13a1d77d013a57
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-04-14 15:22:38 +0530

    feedback form submission

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import plotly.express as px 
+from database import Feedback
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
@@ -7,6 +10,11 @@ app = Flask(__name__)
 def load_data():
     df = px.data.iris()
     return df
+
+def getdb():
+    engine = create_engine('sqlite:///app.sqlite', echo=True)
+    db = scoped_session(sessionmaker(bind=engine))
+    return db
 
 @app.route('/')
 def index():
@@ -23,6 +31,22 @@ def graph_1():
     return render_template('graph_1.html', 
                            fig1= fig.to_html(),
                            fig2= fig2.to_html())
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        if len(name) == 0 or len(message) == 0:
+            print('Name or message is empty')
+        else:
+            db = getdb()
+            db.add(Feedback(name=name, email=email, message=message))
+            db.commit()
+            db.close()
+            return redirect('/feedback')
+    return render_template('feedback.html')
 
 
 if __name__ == '__main__':
